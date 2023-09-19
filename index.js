d0be25caf029c084e1fd7748e54ef6ed941de85a
agnbart <polpumi@gmail.com>
2023-09-19 12:17:55 +0200

    connecting to mongodb

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+
 const app = express();
 
 const PORT = process.env.PORT || 4000;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mydatabase';
+
+const client = new MongoClient(MONGODB_URI);
+
+(async() => {
+    await client.connect();
+    console.log('Baza połączona!');
+    await client.close();
+})();
 
 app.listen(PORT,'localhost',() => {
-    console.log(`DoggieNote server listening on http://localhost:4000`)
+    console.log(`Server listening on http://localhost:${PORT}`)
 })
