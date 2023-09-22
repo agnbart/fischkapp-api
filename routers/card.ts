9b30524283f071c079381053498d584f3a8c1550
agnbart <polpumi@gmail.com>
2023-09-22 15:17:04 +0200

    add delete endpoint, code refactoring

--- routers/card.ts
+++ routers/card.ts
@@ -2,6 +2,7 @@ import {Router} from 'express';
 import {CardClass, CreateCardPayload, UpdateCardPayload} from "../records/card.records";
 import {cardCollection} from "../utils/db";
 import {ObjectId} from "mongodb";
+import {validateCardId} from "../utils/card-search-by-id";
 
 export const cardRouter = Router();
 
@@ -54,6 +55,21 @@ cardRouter
             res.status(500).json({ error: 'Internal Server Error' });
         }
     })
+
+    .delete('/:id', async (req, res) => {
+        const cardId = req.params.id;
+        const existingCard = await validateCardId(cardId, res);
+        const cardObjectId = new ObjectId(cardId);
+
+        try {
+            await cardCollection.deleteOne({_id: cardObjectId});
+            res.status(200).json({success: 'The card has been deleted'})
+        } catch(err) {
+            console.error('Error deleting card from database:', err);
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    })
+
     .post('/', async (req, res) => {
         const {front, back, tags, author}: CreateCardPayload = req.body
 
@@ -77,31 +93,19 @@ cardRouter
     })
 
     .put('/:id', async (req, res) => {
-        const hash = req.params.id;
-
-        let objectId;
-        try {
-            objectId = new ObjectId(hash);
-        } catch (error) {
-            res.status(400).json({ error: 'Invalid ObjectId' });
-            return;
-        }
-        const existingCard = await cardCollection.findOne({ _id: objectId });
-        if (!existingCard) {
-            res.status(404).json({ error: 'Card not found in the database'});
-            throw new Error("Card not found in the database.")
-        }
-
+        const cardId = req.params.id
+        const existingCard = await validateCardId(cardId, res);
+        const cardObjectId = new ObjectId(cardId)
         const {front, back, tags}: UpdateCardPayload = req.body;
         try {
             await cardCollection.updateOne({
-                _id: objectId,
+                _id: cardObjectId,
             },{$set: {
                     front: front,
                     back: back,
                     tags: tags,
                 }})
-            res.status(200).json({ id: hash });
+            res.status(200).json({ id: cardId });
         } catch (err) {
             console.error('Error modifyng card to database:', err);
             res.status(500).json({ error: 'Internal Server Error' });
