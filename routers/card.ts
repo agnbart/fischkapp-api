bde4592b97cbc19094df4bbce752b27264353d75
agnbart <polpumi@gmail.com>
2023-09-22 11:02:42 +0200

    adding put endpoint

--- routers/card.ts
+++ routers/card.ts
@@ -1,6 +1,7 @@
 import {Router} from 'express';
 import {CardClass, CreateCardPayload} from "../records/card.records";
 import {cardCollection} from "../utils/db";
+import {ObjectId} from "mongodb";
 
 export const cardRouter = Router();
 
@@ -25,4 +26,37 @@ cardRouter
             console.error('Error adding card to database:', err);
             res.status(500).json({ error: 'Internal Server Error' });
         }
-    });
+    })
+
+    .put('/:id', async (req, res) => {
+        const hash = req.params.id;
+
+        let objectId;
+        try {
+            objectId = new ObjectId(hash);
+        } catch (error) {
+            res.status(400).json({ error: 'Invalid ObjectId' });
+            return;
+        }
+
+        const existingCard = await cardCollection.findOne({ _id: objectId });
+
+        if (!existingCard) {
+            res.status(400).json({ error: 'Card not found in the database'});
+            throw new Error("Card not found in the database.")
+        }
+
+        try {
+            await cardCollection.updateOne({
+                _id: objectId,
+            },{$set: {
+                    front: req.body.front,
+                    back: req.body.back,
+                    tags: req.body.tags,
+                }})
+            res.status(200).json({ id: hash });
+        } catch (err) {
+            console.error('Error modifyng card to database:', err);
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    })
